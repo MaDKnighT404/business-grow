c5abf7ce1d3ed940b65e3ec4efac36da9a2ad7d2
George Koloidi <Battleaxe@bk.ru>
2023-03-04 02:18:57 +0100

    feat: add darkmode

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,35 @@
-function burgerMenu() {
+function changeburgerMenu() {
   const menu = document.querySelector('#menu');
   const burger = document.querySelector('.burger');
 
   burger.addEventListener('click', () => {
     burger.classList.toggle('active');
-    menu.classList.toggle('hidden')
-    menu.classList.toggle('flex')
-    setTimeout(() => menu.classList.toggle('opacity-0') , 0)
-  })
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('flex');
+    setTimeout(() => menu.classList.toggle('opacity-0'), 0);
+  });
+}
+changeburgerMenu();
+
+// dark mode
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
 }
 
-burgerMenu()
+function changeMode() {
+  const modeBtn = document.querySelector('#mode');
+
+  modeBtn.addEventListener('click', () => {
+    document.querySelector('html').classList.toggle('dark');
+    document.querySelector('html').classList.contains('dark')
+      ? (localStorage.theme = 'dark')
+      : (localStorage.theme = 'light');
+  });
+}
+
+changeMode();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./dist/*.html'],
+  darkMode: 'class',
   theme: {
     screens: {
       xl: { max: "1279px" },
